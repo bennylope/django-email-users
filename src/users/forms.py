659d015d4c13b38656a18474b543f1e46d353ccf
Ben Lopatin <ben@benlopatin.com>
2024-03-10 11:04:27 -0400

    Replace ugettext_lazy with gettext_lazy

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,7 +1,7 @@
 import django
 from django import forms
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
--- src/users/models.py
+++ src/users/models.py
@@ -1,7 +1,7 @@
 from django.core.mail import send_mail
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 
 
